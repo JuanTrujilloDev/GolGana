0dca81e7896876e871ce07a3f8c89001c7856061
Sarsu12 <57489455+Sarsu12@users.noreply.github.com>
2021-08-24 11:47:47 -0500

    Index Update.

--- Desarrollo/GolGana/GolGana/urls.py
+++ Desarrollo/GolGana/GolGana/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.urls import post_patterns
-
+from .views import Home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(post_patterns)),
     path('', include('social_django.urls', namespace='social')),
+    path('', Home.as_view(), name="home")
 ]
